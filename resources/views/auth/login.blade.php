26dfcf3d54dd49b6c34b8a92b95030f84478d528
Mousiya <mousiya29@gmail.com>
2022-06-20 10:55:17 +0530

    Set Register Login templetes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,48 +1,60 @@
 <x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
-
-        <x-jet-validation-errors class="mb-4" />
-
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
+<div class="main">
+      <div class="shop_top">
+		<div class="container">
+            <div class="wrap-breadcrumb">
+                <ul>
+                    <li class="item-link"><a href="/" class="link">home</a></li>
+                    <li class="item-link"><span>login</span></li>
+                </ul>
             </div>
-        @endif
-
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
-
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-jet-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-jet-button class="ml-4">
-                    {{ __('Log in') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
+        
+        
+            <div class=" main-content-area">
+                <div class="wrap-login-item ">						
+                    <div class="login-form form-item form-stl">
+                        <div class="col-md-6">
+                            <div class="login-page">
+                                <fieldset class="wrap-title">
+                                <h1 class="form-title">New Customers</h1>
+                                </fieldset>
+                                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis</p>
+                                <a href="{{route('register')}}">
+                                    <input type="submit" class="btn btn-submit" name="Submit" value="Create an Account"></a>
+                                <div class="clear"></div>
+                            </div>
+                        </div>
+                        
+                        <div class="col-md-6">
+                        <x-jet-validation-errors class="mb-4" />
+                            <form name="frm-login" method="POST" action="{{route('login')}}">
+                                @csrf
+                                <fieldset class="wrap-title">
+                                    <h1 class="form-title">Registered Customer</h1>										
+                                </fieldset>
+                                <fieldset class="wrap-input">
+                                    <label for="frm-login-uname">Email Address:</label>
+                                    <input type="email" id="frm-login-uname" name="email" placeholder="Type your email address":value="old('email')" required autofocus>
+                                </fieldset>
+                                <fieldset class="wrap-input">
+                                    <label for="frm-login-pass">Password:</label>
+                                    <input type="password" id="frm-login-pass" name="password" placeholder="************" required autocomplete="current-password">
+                                </fieldset>
+                                
+                                <fieldset class="wrap-input">
+                                    <label class="remember-field">
+                                        <input class="frm-input " name="remember" id="rememberme" value="forever" type="checkbox"><span>Remember me</span>
+                                    </label>
+                                    <a class="link-function left-position" href="{{route('password.request')}}" title="Forgotten password?">Forgotten password?</a>
+                                </fieldset>
+                                <input type="submit" class="btn btn-submit" value="Login" name="submit">
+                            </form>
+                        </div>
+                    </div>												
+                </div>
+            </div>	
+        
+			</div>
+		  </div>
+	  </div>
 </x-guest-layout>
